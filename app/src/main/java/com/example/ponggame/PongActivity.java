c9901666ad7b9b132ac89ca723a9b4422d8e1813
lseldon16 <lseldon@clemson.edu>
2021-07-25 15:14:29 -0400

    ball and bat

--- app/src/main/java/com/example/ponggame/PongActivity.java
+++ app/src/main/java/com/example/ponggame/PongActivity.java
@@ -3,13 +3,15 @@ package com.example.ponggame;
 
 import android.app.Activity;
 import android.view.Window;
-//import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.graphics.Point;
+import android.view.Display;
 
 
 
 public class PongActivity extends Activity
 {
+    private PongGame mPongGame;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,5 +23,41 @@ public class PongActivity extends Activity
               //  FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
 
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+
+        //initialize pong game, passing things into constructor
+        //this is reference to PongActivity
+        mPongGame = new PongGame(this, size.x, size.y);
+        setContentView(mPongGame);
+
     }
+
+
+
+
+
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+
+        // More code here later in the chapter
+        mPongGame.resume();
+
+    }
+
+
+    @Override
+    protected void onPause()
+    {
+        super.onPause();
+
+        // More code here later in the chapter
+        mPongGame.pause();
+    }
+
+
+
 }
